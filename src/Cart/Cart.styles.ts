44333c1b79ace36946044383caadb369e008b717
maureS <mauroaf4@gmail.com>
2021-06-13 18:03:32 -0300

    my first commit

--- src/Cart/Cart.styles.ts
+++ src/Cart/Cart.styles.ts
@@ -4,4 +4,12 @@ export const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
   width: 500px;
   padding: 20px;
+  @media (max-width: 480px) {
+    width: 300px;
+    padding: 12px;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    width: 400px;
+    padding: 15px;
+  }
 `;
